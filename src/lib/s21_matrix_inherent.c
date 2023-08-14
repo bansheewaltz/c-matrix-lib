5ac7eb0d611811c633d657f7b3f37dc8d6fbf2f3
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-08-15 05:03:11 +0700

    inverse_matrix ready

--- src/lib/s21_matrix_inherent.c
+++ src/lib/s21_matrix_inherent.c
@@ -48,7 +48,7 @@ int s21_determinant(matrix_t *A, double *result) {
   return RC_OK;
 }
 
-// Compute the cofactors matrix, the Hadamard multiplication (element-wise
+// Compute the cofactor matrix, the Hadamard multiplication (element-wise
 // product) of the minors matrix with an alternating grid of +1 and −1.
 int s21_calc_complements(matrix_t *A, matrix_t *result) {
   if (A == NULL || A->matrix == NULL || result == NULL)
@@ -61,46 +61,51 @@ int s21_calc_complements(matrix_t *A, matrix_t *result) {
   rc = s21_create_matrix(A->columns, A->rows, result);
   if (rc != RC_OK)
     return rc;
-
   rc = s21_calc_minors(A, result);
   if (rc != RC_OK)
     return rc;
-
   // Hadamard multiplication
   int matrix_size = A->rows * A->rows;
   for (int i = 1; i < matrix_size; i += 2) {
     result->matrix[i / A->rows][i % A->columns] *= -1;
   }
 
-  return rc;
+  return RC_OK;
 }
 
 int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
   if (A == NULL || A->matrix == NULL || result == NULL)
     return RC_NULL_POINTER_INPUT;
+  if (!s21_is_invertable(A))
+    return RC_CALCULATIONS_CANNOT_BE_PERFORMED;
 
-  int err = 0;
-  err = (A->columns != A->rows);
-  double det = 0.0;
-  s21_determinant(A, &det);
-  if (det == 0 && !err) {
-    err = 2;
-  }
-  if (!err) {
-    if (A->columns == 1) {
-      s21_create_matrix(1, 1, result);
-      result->matrix[0][0] = 1.0 / A->matrix[0][0];
-    } else {
-      matrix_t tmp_minor, tmp_trans;
-      s21_calc_complements(A, &tmp_minor);
-      s21_transpose(&tmp_minor, &tmp_trans);
-      err = s21_mult_number(&tmp_trans, 1.0 / det, result);
-      s21_remove_matrix(&tmp_minor);
-      s21_remove_matrix(&tmp_trans);
-    }
-  } else {
-    s21_create_matrix(A->rows, A->columns, result);
+  int rc = RC_OK;
+
+  int matrix_size = 0;
+  (void)s21_matrix_size(A, &matrix_size);
+
+  if (matrix_size == 1) {
+    rc = s21_create_matrix(1, 1, result);
+    if (rc != RC_OK)
+      return rc;
+    result->matrix[0][0] = 1.0 / A->matrix[0][0];
+    return RC_OK;
   }
 
-  return err;
+  // general case of an invertable matrix
+  matrix_t cofactor;
+  rc = s21_calc_complements(A, &cofactor);
+  if (rc != RC_OK)
+    return rc;
+  matrix_t transpose;
+  rc = s21_transpose(&cofactor, &transpose);
+  if (rc != RC_OK)
+    return rc;
+  s21_remove_matrix(&cofactor);
+  rc = s21_mult_number(&transpose, 1.0 / det, result);
+  if (rc != RC_OK)
+    return rc;
+  s21_remove_matrix(&transpose);
+
+  return RC_OK;
 }
