772cfddae58451bcfd7e8530e4542374d43246a8
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-08-14 20:24:20 +0700

    supplementary functions: slight adjustment of comments

--- src/lib/headers/s21_matrix_supplementary.h
+++ src/lib/headers/s21_matrix_supplementary.h
@@ -14,11 +14,11 @@ int s21_calc_minors(matrix_t *A, matrix_t *result);
 int s21_extract_submatrix(matrix_t *A, int r, int c, matrix_t *result);
 
 // Checks that the matrix is square (rows count is equal to columns count).
-// Also, returns false if the matrix pointer is NULL.
+// Also, returns false if the matrix pointer argument is NULL.
 bool s21_is_square(matrix_t *A);
 
 // Checks that the two matrices are the same size.
-// Also, returns false if any of the pointers is NULL.
+// Also, returns false if any of the arguments of pointer type is NULL.
 bool s21_are_the_same_size(matrix_t *A, matrix_t *B);
 
 #endif  // S21_MATRIX_SUPPLEMENTARY_H_
