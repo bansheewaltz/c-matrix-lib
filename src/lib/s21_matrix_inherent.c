29997909bf83f4d3ea56a7e1d8bcf9acd365a838
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-08-16 02:32:51 +0700

    main functions finalized without testing so far

--- src/lib/s21_matrix_inherent.c
+++ src/lib/s21_matrix_inherent.c
@@ -25,26 +25,35 @@ int s21_determinant(matrix_t *A, double *result) {
   if (!s21_is_square(A))
     return RC_CALCULATIONS_CANNOT_BE_PERFORMED;
 
-  int matrix_size = A->columns;
-  if (matrix_size == 1) {
+  const int side_len = A->columns;
+
+  if (side_len == 1) {
     *result = A->matrix[0][0];
     return RC_OK;
   }
+  if (s21_is_diagonal(A)) {
+    double diagonal_product = 1.0;
+    for (int i = 0; i < side_len; i++) {
+      diagonal_product *= A->matrix[i][i];
+    }
+    *result = diagonal_product;
+    return RC_OK;
+  }
 
+  // permutations method for a general case of a square matrix
   double pos_diagonal_sum = 0.0;
   double neg_diagonal_sum = 0.0;
-  for (int i = 0; i < A->columns; i++) {
+  for (int i = 0; i < side_len; i++) {
     double pos_diagonal = 1.0;
     double neg_diagonal = 1.0;
-    for (int j = 0; j < matrix_size; j++) {
-      pos_diagonal *= A->matrix[(i + j) % matrix_size][(i + j) % matrix_size];
-      neg_diagonal *= A->matrix[(i - j) % matrix_size][(i - j) % matrix_size];
+    for (int j = 0; j < side_len; j++) {
+      pos_diagonal *= A->matrix[(i + j) % side_len][(i + j) % side_len];
+      neg_diagonal *= A->matrix[(i - j) % side_len][(i - j) % side_len];
     }
     pos_diagonal_sum += pos_diagonal;
     neg_diagonal_sum += neg_diagonal;
   }
   *result = pos_diagonal_sum - neg_diagonal_sum;
-
   return RC_OK;
 }
 
@@ -76,15 +85,20 @@ int s21_calc_complements(matrix_t *A, matrix_t *result) {
 int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
   if (A == NULL || A->matrix == NULL || result == NULL)
     return RC_NULL_POINTER_INPUT;
-  if (!s21_is_invertable(A))
-    return RC_CALCULATIONS_CANNOT_BE_PERFORMED;
+  if (!s21_is_square(A))
+    return RC_INCORRECT_MATRIX;
 
   int rc = RC_OK;
 
-  int matrix_size = 0;
-  (void)s21_matrix_size(A, &matrix_size);
+  bool is_invertible = false;
+  double determinant = 0.0;
+  rc = s21_is_invertible(A, &is_invertible, &determinant);
+  if (rc != RC_OK)
+    return rc;
+  if (is_invertible == false)
+    return RC_INCORRECT_MATRIX;
 
-  if (matrix_size == 1) {
+  if (s21_matrix_size(A) == 1) {
     rc = s21_create_matrix(1, 1, result);
     if (rc != RC_OK)
       return rc;
@@ -94,10 +108,10 @@ int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
 
   // general case of an invertable matrix
   matrix_t adjugate;
-  rc = s21_transpose(A, &adjugate);
+  rc = s21_adjugate(A, &adjugate);
   if (rc != RC_OK)
     return rc;
-  rc = s21_mult_number(&adjugate, 1.0 / det, result);
+  rc = s21_mult_number(&adjugate, 1.0 / determinant, result);
   if (rc != RC_OK)
     return rc;
   return RC_OK;
