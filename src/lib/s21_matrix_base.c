99c7d08acb19804c461a6b23806aba341a66e652
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-08-18 00:33:17 +0700

    func matrix_create now properly handles edge cases in terms of memory

--- src/lib/s21_matrix_base.c
+++ src/lib/s21_matrix_base.c
@@ -3,6 +3,9 @@
 
 #include "s21_matrix.h"
 
+// In case of a failure in malloc, cleans up already allocated matrix arrays
+void s21_cleanup_matrix(matrix_t *A);
+
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
   if (result == NULL)
     return RC_NULL_POINTER_INPUT;
@@ -12,13 +15,12 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
   result->matrix = calloc((unsigned)rows, sizeof(double *));
 #ifdef TESTING
   if (rows == 137 && columns == 139) {
+    free(result->matrix);
     result->matrix = NULL;
   }
 #endif
   if (result->matrix == NULL)
     return RC_MEMORY_ALLOCATION_FAILED;
-
-  int rc = RC_OK;
 
   result->rows = rows;
   result->columns = columns;
@@ -26,17 +28,18 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
   for (int i = 0; i < rows; i++) {
     result->matrix[i] = calloc((unsigned)columns, sizeof(double));
 #ifdef TESTING
-    if (rows == 139 && columns == 137) {
+    if (rows == 139 && columns == 137 && i > 0) {
+      free(result->matrix[i]);
       result->matrix[i] = NULL;
     }
 #endif
     if (result->matrix[i] == NULL) {
-      rc = RC_MEMORY_ALLOCATION_FAILED;
-      break;
+      s21_cleanup_matrix(result);
+      return RC_MEMORY_ALLOCATION_FAILED;
     }
   }
 
-  return rc;
+  return RC_OK;
 }
 
 void s21_remove_matrix(matrix_t *A) {
@@ -45,6 +48,13 @@ void s21_remove_matrix(matrix_t *A) {
   if (A->matrix == NULL)
     return;
   for (int i = 0; i < A->rows; i++) {
+    free(A->matrix[i]);
+  }
+  free(A->matrix);
+}
+
+void s21_cleanup_matrix(matrix_t *A) {
+  for (int i = 0; A->matrix[i] != NULL; i++) {
     free(A->matrix[i]);
   }
   free(A->matrix);
