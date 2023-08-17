bcba3c9dcfac8187bd17dfcfa9ea47e2aa542dcf
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-08-18 00:32:00 +0700

    func comparison simplified

--- src/lib/s21_matrix_comparison.c
+++ src/lib/s21_matrix_comparison.c
@@ -12,7 +12,8 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
     return FAILURE;
   for (int i = 0; i < A->rows; i++) {
     for (int j = 0; j < A->columns; j++) {
-      if (fabs(A->matrix[i][j] - B->matrix[i][j]) > EPS_EQ) {
+      double error = fabs(A->matrix[i][j] - B->matrix[i][j]);
+      if (error > EPS_EQ) {
         return FAILURE;
       }
     }
